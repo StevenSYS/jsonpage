043b4db4975bc7f6d95a546a964383c7013a5f57
StevenSYS <stevensys510@gmail.com>
2024-06-15 04:28:34 +0000

    I did the LGPLv3 thing wrong.

--- jsonread.js
+++ jsonread.js
@@ -1,3 +1,20 @@
+/*
+	This file is part of JSONpage.
+
+	JSONpage is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+    JSONpage is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License along with JSONpage.
+	If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const URLParams = new URLSearchParams(window.location.search);
 if (!URLParams.has("jsonFile") && sessionStorage.getItem("jsonFile") == null) {
 	window.location.href = "index.html";
